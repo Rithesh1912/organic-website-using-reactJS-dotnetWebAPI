b22048b2a067056ba8df9c1b5ba4b23574a4ed27
Rithesh S <115693163+Rithesh1912@users.noreply.github.com>
2026-03-26 15:19:06 +0530

    Update config-overrides.js
    added "buffer" and "process" for successfull pipeline build.

--- OrganicShopFinalProject/config-overrides.js
+++ OrganicShopFinalProject/config-overrides.js
@@ -1,15 +1,22 @@
 const webpack = require('webpack');
 
 module.exports = function override(config, env) {
-  // Add polyfills for Node.js core modules
   config.resolve.fallback = {
     http: require.resolve('stream-http'),
     https: require.resolve('https-browserify'),
     stream: require.resolve('stream-browserify'),
     zlib: require.resolve('browserify-zlib'),
-    // Add other node core modules here as needed
+    buffer: require.resolve('buffer'),
+    process: require.resolve('process/browser'),
   };
 
-  // You can also add plugins, loaders, etc. here.
+  config.plugins = [
+    ...(config.plugins || []),
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+      Buffer: ['buffer', 'Buffer'],
+    }),
+  ];
+
   return config;
 };
