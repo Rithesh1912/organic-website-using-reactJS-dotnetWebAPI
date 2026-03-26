e8c80dac6ef4f72310b0ea67318f54068867da16
Rithesh S <115693163+Rithesh1912@users.noreply.github.com>
2026-03-26 20:45:37 +0530

    Update config-overrides.js

--- OrganicShopFinalProject/config-overrides.js
+++ OrganicShopFinalProject/config-overrides.js
@@ -1,19 +1,20 @@
 const webpack = require('webpack');
 
 module.exports = function override(config, env) {
+
   config.resolve.fallback = {
     http: require.resolve('stream-http'),
     https: require.resolve('https-browserify'),
     stream: require.resolve('stream-browserify'),
     zlib: require.resolve('browserify-zlib'),
     buffer: require.resolve('buffer'),
-    process: require.resolve('process'), 
+    process: require.resolve('process/browser'),
   };
 
   config.plugins = [
     ...(config.plugins || []),
     new webpack.ProvidePlugin({
-      process: 'process',   
+      process: 'process/browser', 
       Buffer: ['buffer', 'Buffer'],
     }),
   ];
